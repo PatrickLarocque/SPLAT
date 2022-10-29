335b0f019d13f9faf6434e68ed447a83e8f31ff7
Patrick <patricklarocque1@gmail.com>
2022-10-29 00:51:17 -0400

    UI and Project Structure progress

---
+++ SPLAT/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using SPLAT.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPLAT.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand DashboardViewCommand { get; set; }
+        public RelayCommand TicketsViewCommand { get; set; }
+        public DashboardViewModel DashboardViewModel { get; set; }
+
+        public TicketsViewModel TicketsViewModel { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            DashboardViewModel = new DashboardViewModel();
+            TicketsViewModel = new TicketsViewModel();
+            CurrentView = DashboardViewModel;
+
+            DashboardViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DashboardViewModel;
+            });
+
+            TicketsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = TicketsViewModel;
+            });
+
+        }
+    }
+}
