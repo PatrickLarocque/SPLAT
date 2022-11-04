2653a3c89cf4a0fcd168349ab7fe492b4421cfb4
Mikael1287 <mikael747@gmail.com>
2022-11-04 17:47:17 -0400

    tickets page

--- SPLAT/MVVM/ViewModel/TicketsViewModel.cs
+++ SPLAT/MVVM/ViewModel/TicketsViewModel.cs
@@ -1,12 +1,273 @@
-﻿using System;
+﻿using SPLAT.Core;
+using SPLAT.MVVM.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using FireSharp.Config;
+using Firebase.Database;
+using Firebase.Database.Query;
 
 namespace SPLAT.MVVM.ViewModel
 {
-    internal class TicketsViewModel
+    public class TicketsViewModel : ObservableObject
     {
+        private string _title;
+        private string _description;
+        private string _user_id;
+        private string _Name;
+        private TicketModel _selectedTicket;
+        private List<TicketModel> _tickets = new List<TicketModel>();
+        private List<string> _strTickets = new List<string>();
+        private ObservableCollection<string> _stringCollection = new ObservableCollection<string>();
+        private ICommand _addTicketCommand;
+        private ICommand _deleteTicketCommand;
+        private ICommand _showTicketCommand;
+        private ObservableCollection<TicketModel> _dbTickets = new ObservableCollection<TicketModel>();
+        private delegate ObservableCollection<string> TicketDelegate(IReadOnlyCollection<FirebaseObject<TicketModel>> dbTickets);
+        FirebaseConfig ifc = new FirebaseConfig()
+        {
+            AuthSecret = "uJWNBumr3wGD2xBWuJylCByI238krzBZZoHm1kXc",
+            BasePath = "https://splattest-68d44-default-rtdb.firebaseio.com/"
+        };
+        FirebaseClient _fbclient;
+
+
+        public string Title
+        {
+            get => _title; set
+            {
+                if (value != _title)
+                {
+                    _title = value;
+                    OnPropertyChanged("Title");
+                }
+            }
+        }
+        public string Description
+        {
+            get => _description; set
+            {
+                if (value != _description)
+                {
+                    _description = value;
+                    OnPropertyChanged("Description");
+                }
+            }
+        }
+        public string User_id
+        {
+            get => _user_id; set
+            {
+                if (value != _user_id)
+                {
+                    _user_id = value;
+                    OnPropertyChanged("User_id");
+                }
+            }
+        }
+        public string Name
+        {
+            get => _Name; set
+            {
+                if (value != _Name)
+                {
+                    _Name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        public TicketModel SelectedTicket
+        {
+            get => _selectedTicket; set
+            {
+                if (value != _selectedTicket)
+                {
+                    _selectedTicket = value;
+                    OnPropertyChanged("SelectedTicket");
+                }
+            }
+        }
+        public List<TicketModel> Tickets
+        {
+            get { return _tickets; }
+            set
+            {
+                if (value != _tickets)
+                {
+                    _tickets = value;
+                    OnPropertyChanged("Tickets");
+                }
+            }
+        }
+
+        public List<string> strTickets
+        {
+            get { return _strTickets; }
+            set
+            {
+                if (value != _strTickets)
+                {
+                    _strTickets = value;
+                    OnPropertyChanged("strUsers");
+                }
+            }
+        }
+
+        public ObservableCollection<TicketModel> DbTickets
+        {
+            get { return _dbTickets; }
+            set
+            {
+                if (value != _dbTickets)
+                {
+                    _dbTickets = value;
+                    OnPropertyChanged("DbTickets");
+                }
+            }
+        }
+
+        public ObservableCollection<string> StrCollection
+        {
+            get { return _stringCollection; }
+            set
+            {
+                if (value != _stringCollection)
+                {
+                    _stringCollection = value;
+                    OnPropertyChanged("StrCollection");
+                }
+            }
+        }
+
+        public ICommand AddTicketCommand
+        {
+            get
+            {
+                if (_addTicketCommand == null)
+                {
+                    _addTicketCommand = new RelayCommand(
+                        param => AddTicket());
+                }
+                return _addTicketCommand;
+            }
+        }
+
+
+
+        public ICommand DeleteTicketCommand
+        {
+            get
+            {
+                if (_deleteTicketCommand == null)
+                {
+                    _deleteTicketCommand = new RelayCommand(
+                        param => DeleteTicket());
+                }
+                return _deleteTicketCommand;
+            }
+        }
+
+
+
+        public ICommand ShowTicketCommand
+        {
+            get
+            {
+                if (_showTicketCommand == null)
+                {
+                    _showTicketCommand = new RelayCommand(
+                        param => GetTickets());
+                }
+                return _showTicketCommand;
+            }
+
+        }
+
+        public FirebaseClient FBClient
+        {
+            get { return _fbclient; }
+        }
+
+
+        public TicketsViewModel()
+        {
+            Task.Run(() => GetTickets());
+            {
+                try
+                {
+                    _fbclient = new FirebaseClient("https://splattest-68d44-default-rtdb.firebaseio.com/");
+                }
+                catch
+                {
+                    MessageBox.Show("No Internet or Connection Problem");
+                }
+            }
+        }
+
+
+
+
+        private async void AddTicket()
+        {
+            TicketModel newTicket = new TicketModel();
+            newTicket.Title = Title;
+            Title = string.Empty;
+            newTicket.Description = Description;
+            Description = string.Empty;
+            newTicket.User_id = User_id;
+            User_id = string.Empty;
+            newTicket.Name = Name;
+            Name = string.Empty;
+
+
+            await FBClient
+                .Child("Tickets")
+                .PostAsync(newTicket, false);
+            strTickets.Add("one more");
+            GetTickets();
+        }
+        private async void DeleteTicket()
+        {
+            if (SelectedTicket != null && SelectedTicket.Key != null)
+            {
+                await FBClient.Child("Tickets").Child(SelectedTicket.Key).DeleteAsync();
+                GetTickets();
+            }
+        }
+        private async void GetTickets()
+        {
+            try
+            {
+                var temp = await FBClient.Child("Tickets").OrderByKey().OnceAsync<TicketModel>();
+                await App.Current.Dispatcher.BeginInvoke((Action)delegate () { DbTickets.Clear(); });
+
+                foreach (var e in temp)
+                {
+                    await App.Current.Dispatcher.BeginInvoke((Action)delegate ()
+                    {
+                        DbTickets.Add(new TicketModel
+                        {
+                            Key = e.Key,
+                            Title = e.Object.Title,
+                            Description = e.Object.Description,
+                            User_id = e.Object.User_id,
+                            Name = e.Object.Name
+                        });
+                    });
+                }
+            }
+            catch (Exception)
+            { }
+        }
+        public void Window_ContentRendered(object sender, EventArgs e)
+        {
+
+        }
     }
 }
